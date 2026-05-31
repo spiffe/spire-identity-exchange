44b1f6eeb1791d585b4a8c11245bc7501d69ed20
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-31 07:46:43 -0700

    Try this
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -17,12 +17,12 @@ fi
 teardown() {
   echo ---------------------------
   echo "::group::Status Output"
+  sudo systemctl status spire-identity-exchange@main.service 2>&1 || true
   sudo systemctl status spire-server@main 2>&1 || true
   sudo spire-server entry show 2>&1 || true
   sudo systemctl status spire-controller-manager@main 2>&1 || true
   sudo systemctl status spire-agent@main 2>&1 || true
   sudo systemctl status spire-agent@six 2>&1 || true
-  sudo systemctl status spire-identity-exchange@main.service 2>&1 || true
 }
 
 trap 'EC=$? && trap - SIGTERM && teardown $EC' SIGINT SIGTERM EXIT
