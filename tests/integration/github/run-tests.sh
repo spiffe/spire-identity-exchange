a064565b837885956e43d84f58fe3e64b8e093be
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-05-31 11:01:39 -0700

    Fix remaining race
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -120,8 +120,7 @@ sudo systemctl start spire-identity-exchange@main.service
 MAX_WAIT=30
 ELAPSED=0
 while true; do
-  curl -s -o /dev/null https://localhost:8443 -cacert /etc/spire/identity-exchange/certs/server.pem
-  if [ $? -eq 0 ]; then
+  if curl -s -o /dev/null https://localhost:8443 -cacert /etc/spire/identity-exchange/certs/server.pem; then
     break
   fi
   if [ $ELAPSED -ge $MAX_WAIT ]; then
