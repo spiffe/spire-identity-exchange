98c3218ada2ed477db4d22029f0bb8aca5bc7ab6
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-06 18:06:07 -0700

    Try out format
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -181,4 +181,5 @@ if [ -n "$GITHUB_TOKEN" ]; then
 	curl -H "Authorization: Bearer ${GITHUB_TOKEN}" -X POST https://localhost:8444/api/v1/svid/github/x509 --cacert /etc/spire/identity-exchange/main/certs/server.pem -sS
 fi
 
-curl -H "Authorization: Bearer ${MOCKHUB_TOKEN}" -X POST https://localhost:8444/api/v1/svid/mockhub/x509 --cacert /etc/spire/identity-exchange/main/certs/server.pem -sS
+curl -H "Authorization: Bearer ${MOCKHUB_TOKEN}" -X POST 'https://localhost:8444/api/v1/svid/mockhub/x509&format=spiffe-fd-tar' --cacert /etc/spire/identity-exchange/main/certs/server.pem -sS | tar -xvf -
+more x509/0/* | cat
