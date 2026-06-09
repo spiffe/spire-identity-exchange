372ddf493683be2c735b4972c7f89cf883a6b501
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-09 13:06:31 -0700

    oidc discovery provider bits
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -212,8 +212,8 @@ kubectl create clusterrolebinding spire-identity-exchange --clusterrole=spire-id
 docker exec -i chart-testing-control-plane bash -c 'kubeadm kubeconfig user --client-name=spire-identity-exchange' > "spire-identity-exchange.kubeconfig"
 yq -i '.users[] |= select(.name == "spire-identity-exchange").user |= (del(."client-certificate-data", ."client-key-data") | .exec = {"apiVersion": "client.authentication.k8s.io/v1", "command": "k8s-spiffe-workload-jwt-exec-auth", "interactiveMode": "Never", "env": [{"name": "SPIFFE_JWT_AUDIENCE", "value": "k8s-main"}]})' spire-identity-exchange.kubeconfig
 cat spire-identity-exchange.kubeconfig
-kubectl get nodes --kubeconfig spire-identity-exchange.kubeconfig || true
 sudo mv spire-identity-exchange.kubeconfig /etc/spire/identity-exchange
-kubectl --kubeconfig=/etc/spire/identity-exchange/spire-identity-exchange.kubeconfig get --raw /.well-known/openid-configuration
-kubectl --kubeconfig=/etc/spire/identity-exchange/spire-identity-exchange.kubeconfig get --raw /openid/v1/jwks
+sudo systemd-run --wait --unit=spire-identity-exchange-job $(which kubectl) get nodes --kubeconfig /etc/spire/identity-exchange/spire-identity-exchange.kubeconfig
+#kubectl --kubeconfig=/etc/spire/identity-exchange/spire-identity-exchange.kubeconfig get --raw /.well-known/openid-configuration
+#kubectl --kubeconfig=/etc/spire/identity-exchange/spire-identity-exchange.kubeconfig get --raw /openid/v1/jwks
 
