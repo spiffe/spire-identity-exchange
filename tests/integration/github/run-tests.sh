e4879dc113cd548c4c50b003409d60079cf0f526
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-09 16:46:15 -0700

    oidc discovery provider bits
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -149,6 +149,8 @@ docker exec -i chart-testing-control-plane bash -c 'cat /etc/kubernetes/pki/auth
 docker exec -i chart-testing-control-plane bash -c 'cat /etc/hosts'
 docker exec -i chart-testing-control-plane bash -c 'curl -k https://k8ssodp.example.org:8181/.well-known/openid-configuration'
 
+sudo cat /etc/kubernetes/manifests/kube-apiserver.yaml
+
 sudo mkdir -p /usr/libexec/spire/
 sudo cp -a build/bin/spire-identity-exchange /usr/libexec/spire/
 
