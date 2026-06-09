0bc30ab3d3c5337e82021037c8503a96e9999c34
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-09 12:40:20 -0700

    oidc discovery provider bits
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -210,6 +210,7 @@ kubectl apply -f "${SCRIPTPATH}/../../../k8s/spire-identity-exchange-clusterrole
 kubectl create clusterrolebinding spire-identity-exchange --clusterrole=spire-identity-exchange --user=spire-identity-exchange
 
 docker exec -i chart-testing-control-plane bash -c 'kubeadm kubeconfig user --client-name=spire-identity-exchange' > "spire-identity-exchange.kubeconfig"
+yq -i '( .users[] | select(.name == "spire-identity-exchange") .user ) |= ( del(.client-certificate-data, .client-key-data) + {"exec": {"apiVersion": "client.authentication.k8s.io/v1", "command": "k8s-spiffe-workload-jwt-exec-auth", "interactiveMode": "Never", "env": [{"name": "SPIFFE_JWT_AUDIENCE", "value": "k8s-main"}]}} )' spire-identity-exchange.kubeconfig
 cat spire-identity-exchange.kubeconfig
 kubectl get nodes --kubeconfig spire-identity-exchange.kubeconfig || true
 sudo mv spire-identity-exchange.kubeconfig /etc/spire/identity-exchange
