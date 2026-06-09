6de9b2428f3af9fc71da1f5c8ba936badda27c7f
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-09 15:25:08 -0700

    oidc discovery provider bits
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -208,15 +208,16 @@ fi
 curl -f -H "Authorization: Bearer ${MOCKHUB_TOKEN}" -X POST https://localhost:8444/api/v1/svid/mockhub/x509 --cacert /etc/spire/identity-exchange/main/certs/server.pem -sS
 
 kubectl apply -f "${SCRIPTPATH}/../../../k8s/spire-identity-exchange-clusterrole.yaml"
-kubectl create clusterrolebinding spire-identity-exchange --clusterrole=spire-identity-exchange --user=spire-identity-exchange
+kubectl create clusterrolebinding spire-identity-exchange --clusterrole=spire-identity-exchange --user="spiffe://example.org/service/spire-identity-exchange"
+kubectl create clusterrolebinding spire-identity-exchange-admin --clusterrole=cluster-admin --user="spiffe://example.org/service/spire-identity-exchange"
 
 docker exec -i chart-testing-control-plane bash -c 'kubeadm kubeconfig user --client-name=spire-identity-exchange' > "spire-identity-exchange.kubeconfig"
 yq -i '.users[] |= select(.name == "spire-identity-exchange").user |= (del(."client-certificate-data", ."client-key-data") | .exec = {"apiVersion": "client.authentication.k8s.io/v1", "command": "k8s-spiffe-workload-jwt-exec-auth", "interactiveMode": "Never", "env": [{"name": "SPIFFE_JWT_AUDIENCE", "value": "k8s-main"}, {"name": "SPIFFE_ENDPOINT_SOCKET", "value": "unix:///var/run/spire/agent/sockets/main/public/api.sock"}]})' spire-identity-exchange.kubeconfig
 cat spire-identity-exchange.kubeconfig
 sudo mv spire-identity-exchange.kubeconfig /etc/spire/identity-exchange
-#timeout 10 sudo systemd-run --wait --pipe --unit=spire-identity-exchange-job /bin/strace -f -F -s 10000 $(which kubectl) get nodes --kubeconfig /etc/spire/identity-exchange/spire-identity-exchange.kubeconfig
-t=$(timeout 10 sudo systemd-run --wait --pipe --setenv=SPIFFE_ENDPOINT_SOCKET=unix:///var/run/spire/agent/sockets/main/public/api.sock --unit=spire-identity-exchange-job /bin/k8s-spiffe-workload-jwt-exec-auth | yq e .status.token)
-echo $t | base64
+timeout 10 sudo systemd-run --wait --pipe --unit=spire-identity-exchange-job /bin/strace -f -F -s 10000 $(which kubectl) get nodes --kubeconfig /etc/spire/identity-exchange/spire-identity-exchange.kubeconfig
+#t=$(timeout 10 sudo systemd-run --wait --pipe --setenv=SPIFFE_ENDPOINT_SOCKET=unix:///var/run/spire/agent/sockets/main/public/api.sock --unit=spire-identity-exchange-job /bin/k8s-spiffe-workload-jwt-exec-auth | yq e .status.token)
+#echo $t | base64
 #timeout 10 sudo systemd-run --wait --pipe --unit=spire-identity-exchange-job /bin/ls -l /var/run/spire/agent/sockets/main/public/api.sock
 #kubectl --kubeconfig=/etc/spire/identity-exchange/spire-identity-exchange.kubeconfig get --raw /.well-known/openid-configuration
 #kubectl --kubeconfig=/etc/spire/identity-exchange/spire-identity-exchange.kubeconfig get --raw /openid/v1/jwks
