92c66b29b766943fdffc044bcf7836004f48e6bc
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-10 10:00:43 -0700

    Refactor
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/common/common.sh
+++ tests/integration/common/common.sh
@@ -107,3 +107,57 @@ deploy_credential_composer() {
   /usr/libexec/spire/plugins/credentialcomposer-identity-exchange || true
 }
 
+setup_base_spire() {
+  local SCRIPTPATH="$1"
+  local COMMONPATH="$2"
+
+  # Get the package repo and install the packages
+  sudo curl -s -o /etc/apt/sources.list.d/spire-examples.list https://raw.githubusercontent.com/spiffe/spire-examples/refs/heads/main/examples/debs/amd64/spire-examples.list
+  sudo apt-get update
+  sudo apt-get install -y spire-common spire-agent spire-server spire-controller-manager
+
+  # register some workloads with the spire server using manifests
+  sudo mkdir -p /etc/spire/server/main/manifests/
+  sudo cp "${COMMONPATH}/manifests"/* /etc/spire/server/main/manifests/
+
+  # Startup server and make sure its ready
+  sudo cp "${COMMONPATH}/server.conf" /etc/spire/server/main/config
+  sudo systemctl start spire-server@main spire-controller-manager@main
+  wait_for_healthcheck spire-server /run/spire/server/sockets/main/private/api.sock
+
+  # Configure our agents. For the test, create join token for the main agent. You should really use a node attestor other then join tokens such as tpm-direct, http_challenge, or a cloud provider one
+  JOIN_TOKEN=$(sudo spire-server token generate -spiffeID spiffe://example.org/agent/node1 | awk '{print "\""$2"\""}')
+  export JOIN_TOKEN
+  sudo /bin/bash -c "echo JOIN_TOKEN=${JOIN_TOKEN} > /etc/spire/agent/main.env"
+  sudo cp "${COMMONPATH}/six-agent.conf" /etc/spire/agent/six.conf
+
+  # Startup the agents
+  sudo systemctl start spire-agent@main spire-agent@six
+  wait_for_healthcheck spire-agent /var/run/spire/agent/sockets/main/public/api.sock
+  wait_for_healthcheck spire-agent /var/run/spire/agent/sockets/six/public/api.sock
+}
+
+setup_identity_exchange() {
+  local SCRIPTPATH="$1"
+  local COMMONPATH="$2"
+
+  sudo mkdir -p /usr/libexec/spire/
+  chmod +x spire-identity-exchange
+  sudo cp -a spire-identity-exchange /usr/libexec/spire/
+
+  sudo mkdir -p /etc/spire/identity-exchange/main/certs
+  sudo openssl req -x509 -newkey rsa:2048 \
+    -keyout /etc/spire/identity-exchange/main/certs/server.key \
+    -out /etc/spire/identity-exchange/main/certs/server.pem -sha256 -days 365 -nodes \
+    -subj "/CN=localhost" \
+    -addext "basicConstraints=critical,CA:TRUE" \
+    -addext "subjectAltName=DNS:localhost,DNS:spire-identity-exchange.example.org,IP:127.0.0.1"
+
+  sudo cp "${SCRIPTPATH}/default.json" /etc/spire/identity-exchange/
+  sudo cp "${COMMONPATH}/../../../systemd/spire-identity-exchange@.service" /etc/systemd/system
+  sudo systemctl daemon-reload
+  sudo systemctl start spire-identity-exchange@main.service
+
+  wait_for_spire_identity_exchange
+}
+
--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -64,30 +64,7 @@ sudo mkdir -p /etc/spire/server/main/manifests
 sudo cp "${SCRIPTPATH}/manifests"/* /etc/spire/server/main/manifests/
 
 # Common spire setup bits
-# Get the package repo and install the packages
-sudo curl -s -o /etc/apt/sources.list.d/spire-examples.list https://raw.githubusercontent.com/spiffe/spire-examples/refs/heads/main/examples/debs/amd64/spire-examples.list
-sudo apt-get update
-sudo apt-get install -y spire-common spire-agent spire-server spire-controller-manager
-
-# register some workloads with the spire server using manifests
-sudo mkdir -p /etc/spire/server/main/manifests/
-sudo cp "${SCRIPTPATH}/../common/manifests"/* /etc/spire/server/main/manifests/
-
-# Startup server and make sure its ready
-sudo cp "${SCRIPTPATH}/server.conf" /etc/spire/server/main/config
-sudo systemctl start spire-server@main spire-controller-manager@main
-wait_for_healthcheck spire-server /run/spire/server/sockets/main/private/api.sock
-
-# Configure our agents. For the test, create join token for the main agent. You should really use a node attestor other then join tokens such as tpm-direct, http_challenge, or a cloud provider one
-JOIN_TOKEN=$(sudo spire-server token generate -spiffeID spiffe://example.org/agent/node1 | awk '{print "\""$2"\""}')
-export JOIN_TOKEN
-sudo /bin/bash -c "echo JOIN_TOKEN=${JOIN_TOKEN} > /etc/spire/agent/main.env"
-sudo cp "${SCRIPTPATH}/six-agent.conf" /etc/spire/agent/six.conf
-
-# Startup the agents
-sudo systemctl start spire-agent@main spire-agent@six
-wait_for_healthcheck spire-agent /var/run/spire/agent/sockets/main/public/api.sock
-wait_for_healthcheck spire-agent /var/run/spire/agent/sockets/six/public/api.sock
+setup_base_spire "${SCRIPTPATH}" "${SCRIPTPATH}/../common"
 
 # K8s specific bits
 sudo apt-get install -y k8s-spiffe-workload-auth-config k8s-spiffe-workload-jwt-exec-auth spiffe-helper spiffe-oidc-discovery-provider k8s-spiffe-oidc-discovery-provider
@@ -120,24 +97,7 @@ sudo cat /etc/kubernetes/manifests/kube-apiserver.yaml
 sudo ls /etc/kubernetes/pki/
 
 # Setup spire-identity-exchange
-sudo mkdir -p /usr/libexec/spire/
-chmod +x spire-identity-exchange
-sudo cp -a spire-identity-exchange /usr/libexec/spire/
-
-sudo mkdir -p /etc/spire/identity-exchange/main/certs
-sudo openssl req -x509 -newkey rsa:2048 \
-  -keyout /etc/spire/identity-exchange/main/certs/server.key \
-  -out /etc/spire/identity-exchange/main/certs/server.pem -sha256 -days 365 -nodes \
-  -subj "/CN=localhost" \
-  -addext "basicConstraints=critical,CA:TRUE" \
-  -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"
-
-sudo cp "${SCRIPTPATH}/default.json" /etc/spire/identity-exchange/
-sudo cp "${SCRIPTPATH}/../../../systemd/spire-identity-exchange@.service" /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl start spire-identity-exchange@main.service
-
-wait_for_spire_identity_exchange
+setup_identity_exchange "${SCRIPTPATH}" "${SCRIPTPATH}/../common"
 
 # Tests
 
