3e45d9b247223a26ac5f1b84b1a9017d9dfb22ef
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-10 06:36:36 -0700

    Fix name issue
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -19,7 +19,7 @@ teardown() {
   echo "::group::Status Output"
   kubectl get pods -l job-name=test -o name | xargs kubectl describe || true
   kubectl logs job/test || true
-  sudo journalctl -fu spire-identity-exchange@main.service || true
+  sudo journalctl -u spire-identity-exchange@main.service || true
   kubectl logs -n kube-system kube-apiserver-chart-testing-control-plane || true
   sudo systemctl status spire-identity-exchange-job -n 1000  2>&1 || true
   sudo systemctl status k8s-spiffe-workload-auth-config 2>&1 || true
