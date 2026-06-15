fa569c928320db2080e3fa63ef0b9b00ac48216e
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-15 06:53:51 -0700

    Log more
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -20,6 +20,8 @@ teardown() {
   echo ---------------------------
   echo "::group::Status Output"
   sudo journalctl -u spire-identity-exchange@main.service || true
+  sudo journalctl -u spire-server-attestor-spiffe-workload-api@main.service || true
+  sudo systemctl status spire-server-attestor-spiffe-workload-api@main.service || true
   sudo spire-server agent show -spiffeID spiffe://example.org/spire/agent/x509pop/spire-identity-exchange/node1 || true
   sudo systemctl status spire-identity-exchange@main.service -n 50 2>&1 || true
   sudo systemctl status spire-server@main -n 50 2>&1 || true
