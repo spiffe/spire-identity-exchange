709a41edb269ff012946290498bc82b6162e286e
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-21 15:19:26 -0700

    Some fixes
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -131,7 +131,7 @@ sudo spiffe-step-ssh-get-cert-authority user main | sudo -u spiffe-test tee /hom
 git clone https://github.com/spiffe/spiffe-step-ssh
 cd spiffe-step-ssh
 git checkout spiffe-fd
-git build cmd/spiffe-step-ssh-user-agent
+go build cmd/spiffe-step-ssh-user-agent
 cd ../
 
 export SPIFFE_ENDPOINT="file:///$(pwd)"
