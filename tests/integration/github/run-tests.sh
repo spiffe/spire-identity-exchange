970ab5c805eec1cf3b09192def2e403bb85153a3
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-22 06:33:49 -0700

    Some fixes
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/github/run-tests.sh
+++ tests/integration/github/run-tests.sh
@@ -150,6 +150,7 @@ cd ../
 
 ls -l $(pwd)/x509/0/
 export SPIFFE_ENDPOINT="file://$(pwd)"
+export SPIFFE_STEP_SSH_USER_AGENT_MODE=continuous
 export SPIFFE_STEP_SSH_FETCHCA_URL="https://spiffe-step-ssh-fetchca.example.org:5443"
 export SPIFFE_STEP_SSH_URL="https://spiffe-step-ssh.example.org:7443"
 eval `timeout 60 ./spiffe-step-ssh/spiffe-step-ssh-user-agent`
