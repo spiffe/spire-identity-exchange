3d114713d4037dcc39b66bcee5725faae8bc1afe
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-06-28 18:01:34 -0700

    Updates
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/k8s_deploy/run-tests.sh
+++ tests/integration/k8s_deploy/run-tests.sh
@@ -81,6 +81,8 @@ docker cp temp:/ko-app/spire-credentialcomposer-identity-exchange /tmp/cc
 SUM=$(sha256sum /tmp/cc | awk '{print $1}')
 timeout 120 helm upgrade --install -n spire-server spire helm-charts-hardened/charts/spire -f "${SCRIPTPATH}/spire-values.yaml" --set "spire-server.credentialComposer.spireIdentityExchange.checksum=${SUM}" --wait
 
+sleep 120
+
 kubectl apply -f "${SCRIPTPATH}/test-job.yaml"
 kubectl wait --for=condition=complete --timeout=60s job/test && \
 kubectl logs job/test | base64 -d | tar -xvf -
