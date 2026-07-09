032b909d5c3eb27231fad9219eca1e4129bedf85
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-07-09 13:19:32 -0700

    Fix function
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/k8s_psat/run-tests.sh
+++ tests/integration/k8s_psat/run-tests.sh
@@ -122,5 +122,5 @@ cat crane-config/config.json
 docker pull docker.io/library/busybox:latest
 docker tag docker.io/library/busybox:latest zot.example.org:5000/test/busybox:latest
 docker save zot.example.org:5000/test/busybox:latest -o busybox.tar
-DOCKER_CONFIG=./crane-config ~/go/bin/crane push busybox.tar zot.example.org:5000/test/busybox:latest
+DOCKER_CONFIG=./crane-config ~/go/bin/crane push busybox.tar zot.example.org:5000/test/busybox:latest --insecure
 
