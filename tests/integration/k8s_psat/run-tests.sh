5cf270c47ff5396f8e1fc5fc558466a29fa6290c
Kevin Fox <Kevin.Fox@pnnl.gov>
2026-07-10 16:18:46 -0700

    Fix function
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- tests/integration/k8s_psat/run-tests.sh
+++ tests/integration/k8s_psat/run-tests.sh
@@ -129,7 +129,7 @@ cat <<EOF > crane-config/config.json
 EOF
 
 echo '{"insecure-registries" : ["zot.example.org:5000"]}' | sudo tee /etc/docker/daemon.json
-sudo systemctl restart docker
+sudo systemctl reload docker
 
 cat crane-config/config.json
 docker pull docker.io/library/busybox:latest
